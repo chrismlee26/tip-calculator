afd03853dee3e2be975a219026e8268261e404a9
CHRIS_LEE <chrismlee87@gmail.com>
2020-10-07 19:31:53 -0700

    just css left

--- script.js
+++ script.js
@@ -9,70 +9,14 @@ function calculateTip() {
     const tipValue = tipInput.value
     const peopleValue = peopleInput.value
 
-
     const tipAmount = billValue * tipValue / peopleValue / 100
     display.innerHTML = tipAmount.toFixed(2)
-}
 
-function calculateTotal() {
-    // const addTotal = parseInt(billInput.value) + display.innerHTML
-    // const billTotal = parseFloat(billInput.value)
-    // const displayTotal = parseFloat(display.innerHTML)
-    // const addTotal = billTotal + displayTotal
-
-    const billTotal = billInput.value
-    const displayTotal = display.innerHTML
-    const addTotal = billTotal + displayTotal
-
-    total.innerHTML = addTotal.toFixed(2)
-   
-    console.log(billInput.value, display.innerHTML)
-    console.log(typeof billTotal) 
-    console.log(typeof displayTotal)
-    console.log(typeof addTotal)
+    const totalAmount = billValue * (1 + tipValue / 100)
+    total.innerHTML = totalAmount.toFixed(2)
 }
 
 calculateTip()
 billInput.addEventListener('input', calculateTip)
 tipInput.addEventListener('input', calculateTip)
 peopleInput.addEventListener('input', calculateTip)
-
-
-total.addEventListener('total', calculateTotal)
-calculateTotal()
-
-
-
-
-// Try #1 
-// function calculateTotal() {
-//     const addTotal = parseInt(billInput.value) + parseInt(display.innerHTML)
-//     total.innerHTML = addTotal
-
-//     console.log(addTotal, "addTotal", display.value, billInput.value, parseInt(display.innerHTML))
-// }
-
-// Try #2
-// function calculateTotal() {
-//     // const addTotal = parseInt(billInput.value) + display.innerHTML
-//     const billTotal = parseFloat(billInput.value)
-//     const displayTotal = parseFloat(display.innerHTML)
-//     const addTotal = billTotal + displayTotal
-
-//     console.log(billInput.value, display.innerHTML)
-//     console.log(typeof billTotal) 
-//     console.log(typeof displayTotal)
-//     console.log(typeof addTotal)
-    
-// }
-
-
-// Try #3
-// function calculateTotal() {
-//     const totalBill = billInput.value
-//     const totalTip = tipInput.value
-
-//     const tipTotal = totalBill * totalTip / 100
-//     total.innerHTML = tipTotal.toFixed(2)
-    
-// }
